feb388146f4f0f7c4664a6b7a58aede5a0787077
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-07-13 21:42:37 +0200

    Yelp search

--- controller.js
+++ controller.js
@@ -1,9 +1,92 @@
+'use strict'
 
+const bodyParser = require('body-parser');
+const yelp = require('yelp-fusion');
+const env = require('./env');
 
-module.exports = function(app) {
-	app.get("*", function(req, res) {
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const clientId = env.yelp.CLIENT_ID;
+const clientSecret = env.yelp.CLIENT_SECRET;
+let accessToken;
+
+const token = yelp.accessToken(clientId, clientSecret).then(response => {
+    accessToken = response.jsonBody.access_token;
+}).catch(e => {
+    console.log(e);
+});
+
+module.exports = function(app, passport) {
+	app.get("/", function(req, res) {
+        let user = {};
+        let results = {};
+        if (req.user) {
+            user.name = req.user.displayName;
+            user.id = req.user._json.id_str;
+        }
 		res.render("index", {
-			user: {}
+			user: user,
+            results: results
 		});
 	})
+
+    app.post("/search", urlencodedParser, function(req, res) {
+
+        let user = {};
+        if (req.user) {
+            user.name = req.user.displayName;
+            user.id = req.user._json.id_str;
+        }
+
+        const client = yelp.client(accessToken);
+
+        client.search({
+            categories:'nightlife',
+            location: req.body.query
+        }).then(response => {
+
+            let results = {};
+            results.businesses = response.jsonBody.businesses;
+            res.render("index", {
+                user: user,
+                results: results
+            });
+        }).catch(e => {
+            console.log(e);
+            res.render("index", {
+                user: user,
+                results: {}
+            });
+        });
+    })
+
+	app.get("/signin", function(req, res){
+        twitterAuthenticator(req, res);
+    });
+
+    var twitterAuthenticator = passport.authenticate("twitter");
+
+    app.get("/signout", function(req, res){
+        PollModel.find({}, function(err, polls) {
+            if (err) throw err;
+            var username;
+            if(req.user) username = req.user.username;
+            else username = "user";
+            req.session.destroy();
+            res.locals.user = null;
+            res.render("signout", { user: {}, polls: polls });
+        })
+    });
+
+    var authenticateNewUser = passport.authenticate("twitter", { failureRedirect: "/signout" });
+
+    app.get("/auth/twitter/callback", function(req, res, next){
+            authenticateNewUser(req, res, next);
+        }, function(req, res){
+            newUser = {
+                id: req.user._json.id_str,
+                name: req.user.displayName,
+            }
+            res.redirect('/');
+        });
 }
--- server.js
+++ server.js
@@ -1,12 +1,70 @@
-let express = require("express");
-let app = express();
-let controller = require('./controller');
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const Strategy = require("passport-twitter").Strategy;
+const controller = require('./controller');
+const env = require('./env');
 
 app.set("view engine", "pug");
 
 app.use(express.static('./public'));
 
-controller(app);
+var cookieParserFunction = cookieParser();
+app.use(function(req, res, next){
+    cookieParserFunction(req, res, next);
+});
+
+var sessionFunction = session({
+    secret: env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+});
+
+app.use(function(req, res, next){
+    sessionFunction(req, res, next);
+});
+
+var passportInitializer = passport.initialize();
+app.use(function(req, res, next){
+    passportInitializer(req, res, next);
+});
+
+var passportSessionFunction = passport.session();
+app.use(function(req, res, next){
+    passportSessionFunction(req, res, next);
+});
+
+app.use(function(req, res, next){
+    res.locals.user = req.user;
+    next();
+});
+
+passport.serializeUser(function(user, next) {
+    next(null, user);
+});
+
+passport.deserializeUser(function(user, next) {
+    next(null, user);
+});
+
+let twitterObj = {
+    consumerKey: env.twitter.CONSUMER_KEY,
+    consumerSecret: env.twitter.CONSUMER_SECRET,
+    callbackURL: env.twitter.CALLBACK_URL
+}
+
+var twitterStrategy = new Strategy(
+    twitterObj,
+    function(token, tokenSecret, profile, next){
+        next(null, profile);
+    }
+);
+passport.use(twitterStrategy);
+
+// Set up Routes
+controller(app, passport);
 
 const port = process.env.PORT || 3000;
 
