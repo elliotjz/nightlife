3b5dc119b849d935a92f19c330062222da1562da
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-07-14 16:53:14 +0200

    mlab integration

--- controller.js
+++ controller.js
@@ -2,10 +2,12 @@
 
 const bodyParser = require('body-parser');
 const yelp = require('yelp-fusion');
+const mongoose = require('mongoose');
 const env = require('./env');
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
+// Setup YELP API
 const clientId = env.yelp.CLIENT_ID;
 const clientSecret = env.yelp.CLIENT_SECRET;
 let accessToken;
@@ -16,12 +18,26 @@ const token = yelp.accessToken(clientId, clientSecret).then(response => {
     console.log(e);
 });
 
+// Setup Mongoose & MLAB
+mongoose.connect(env.MLAB_URL);
+let userSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    rsvps: Object
+})
+let rsvpSchema = new mongoose.Schema({
+    id: String,
+    going: Array
+})
+let UserModel = mongoose.model('users', userSchema);
+let RsvpModel = mongoose.model('rsvps', rsvpSchema);
+
 module.exports = function(app, passport) {
 	app.get("/", function(req, res) {
         let user = {};
         let results = {};
         if (req.user) {
-            user.name = req.user.displayName;
+        user.name = req.user.displayName;
             user.id = req.user._json.id_str;
         }
 		res.render("index", {
@@ -31,38 +47,41 @@ module.exports = function(app, passport) {
 	})
 
     app.post("/search", urlencodedParser, function(req, res) {
+        RsvpModel.find({}, function(err, rsvps) {
+            if (err) throw err;
+            let user = {};
+            if (req.user) {
+                user.name = req.user.displayName;
+                user.id = req.user._json.id_str;
+            }
+            // Search
+            const client = yelp.client(accessToken);
 
-        let user = {};
-        if (req.user) {
-            user.name = req.user.displayName;
-            user.id = req.user._json.id_str;
-        }
+            client.search({
+                categories:'nightlife',
+                location: req.body.query
+            }).then(response => {
 
-        const client = yelp.client(accessToken);
-
-        client.search({
-            categories:'nightlife',
-            location: req.body.query
-        }).then(response => {
-
-            let results = {};
-            results.businesses = response.jsonBody.businesses;
-            res.render("index", {
-                user: user,
-                results: results
+                let results = {};
+                results.businesses = response.jsonBody.businesses;
+                res.render("index", {
+                    user: user,
+                    results: results,
+                    rsvps: rsvps
+                });
+            }).catch(e => {
+                console.log(e);
+                res.render("index", {
+                    user: user,
+                    results: {},
+                });
             });
-        }).catch(e => {
-            console.log(e);
-            res.render("index", {
-                user: user,
-                results: {}
-            });
-        });
+        })
     })
 
 	app.get("/signin", function(req, res){
         twitterAuthenticator(req, res);
-    });
+    })
 
     var twitterAuthenticator = passport.authenticate("twitter");
 
@@ -80,13 +99,35 @@ module.exports = function(app, passport) {
 
     var authenticateNewUser = passport.authenticate("twitter", { failureRedirect: "/signout" });
 
-    app.get("/auth/twitter/callback", function(req, res, next){
+    app.get("/auth/twitter/callback", function(req, res, next) {
             authenticateNewUser(req, res, next);
-        }, function(req, res){
-            newUser = {
+        }, function(req, res) {
+            let newUser = {
                 id: req.user._json.id_str,
                 name: req.user.displayName,
+                rsvps: {}
             }
-            res.redirect('/');
-        });
+            UserModel.find({ id: newUser.id }, function(err, data) {
+                if (err) throw err;
+                console.log("data: " + data);
+                console.log("typeof data: " + (typeof data));
+                if (!data) {
+                    let newUserDoc = UserModel(newUser).save(function(err, data) {
+                        if (err) throw err;
+                    })
+                } else {
+                    console.log("user ID: " + newUser.id);
+                    let newRsvp = {"one": 345};
+                    UserModel.update({ id: newUser.id }, {
+                        $set: { rsvps: newRsvp }
+                        }, function(err, data) {
+                            if (err) throw err;
+                        }
+                    )
+                }
+                
+            })
+        }
+    )
 }
+
